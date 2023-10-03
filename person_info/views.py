3f634a26987b78230e1e3c7921c0f9a85c992497
chougalerohit <rchougale5@gmail.com>
2023-10-03 14:44:53 +0530

    index function change by rohit

--- person_info/views.py
+++ person_info/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def index(r):
-    return HttpResponse('<h1>Welcome to rohit second project,thanks for visit...! visit again</h1>')
+    return HttpResponse('<h1>Hi,welcome to rohit second project</h1>')
 
 def show(r):
     return HttpResponse('<h1>hi krish,thanks for visit...!  visit again...!</h1>')
