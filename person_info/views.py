43fa712bfcd9e9b32c7425f694f3018d4b87f68a
chougalerohit <rchougale5@gmail.com>
2023-10-03 16:52:26 +0530

    show function changed by rohit

--- person_info/views.py
+++ person_info/views.py
@@ -7,4 +7,4 @@ def index(r):
     return HttpResponse('<h1>Hi,welcome to rohit second project</h1>')
 
 def show(r):
-    return HttpResponse('<h1>hi krish...!</h1>')
+    return HttpResponse('<h1>hi krish...! what r u doing..?</h1>')
